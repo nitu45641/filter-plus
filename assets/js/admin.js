cbe50ff615460b58ce65b016c144cfc9a939a043
nitu456 <42024930+nitu456@users.noreply.github.com>
2023-12-04 13:31:23 +0600

    css update

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -18,7 +18,7 @@
 					e.preventDefault();
 					let parent_block = _this.parents(".shortcode-block");
 					var shortcode_name = parent_block.data("name");
-					let input_value = findInputValue(parent_block.find(".shortcode-section"));
+					let input_value = findInputValue(parent_block.find(".input-section"));
 
 					let shortcode = `[${shortcode_name} ${input_value}]`; 
 					results.val("").val(shortcode);
