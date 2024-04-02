8b9c5906de2567eb45e29c6ddd0addf4326641fc
nitu456 <42024930+nitu456@users.noreply.github.com>
2024-04-03 00:04:40 +0600

    fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-server-side-render'), 'version' => '2733c292a355c29b7dcf');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-server-side-render'), 'version' => '30ce9b88539e254e3bbf');
