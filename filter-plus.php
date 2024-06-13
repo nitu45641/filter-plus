0e92eb381cc65796de90a9760eedd8af92e29104
nitu456 <42024930+nitu456@users.noreply.github.com>
2024-06-13 13:01:04 +0600

    admin ux improve

--- filter-plus.php
+++ filter-plus.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Filter Plus
  * Plugin URI:        https://woooplugin.com/filter-plus
  * Description:       Advanced Product Filter plugin that enable filter anything features like filter by by Ratings, Tags, Price Range on website. It allows users to filter anything based on different taxonomies.
- * Version:           1.0.62
+ * Version:           1.0.63
  * Requires at least: 5.2
  * Requires PHP:      7.3
  * Author:            Woooplugin
