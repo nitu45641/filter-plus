c3fb4b4ea1d71264aa3c42b4ee0493f63a53273b
nitu208 <farzana@zone7.co>
2024-09-23 18:53:16 +0600

    filter change

--- templates/woo-filter/parts/categories-list.php
+++ templates/woo-filter/parts/categories-list.php
@@ -16,19 +16,19 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 					data-slug="<?php echo esc_attr($item['slug'])?>"
 				>
 					<?php echo esc_html($item['name']);?>
-					<?php if( $sub_categories == 'yes' && !empty($item['sub_categories'])): ?>
-					<ul class="sub_categories">
-					<?php foreach($item['sub_categories'] as $sub): ?>
-						<li 
-							data-name="<?php echo esc_attr($sub['name'])?>"
-							data-cat_id="<?php echo esc_attr($sub['term_id'])?>"
-							data-slug="<?php echo esc_attr($sub['slug'])?>">
-							<?php echo esc_html($sub['name']);?>
-						</li>
-					<?php endforeach; ?>
-					</ul>
-					<?php endif; ?>
 				</li>
+					<?php if( $sub_categories == 'yes' && !empty($item['sub_categories'])): ?>
+						<ul class="sub_categories">
+							<?php foreach($item['sub_categories'] as $sub): ?>
+								<li 
+									data-name="<?php echo esc_attr($sub['name'])?>"
+									data-cat_id="<?php echo esc_attr($sub['term_id'])?>"
+									data-slug="<?php echo esc_attr($sub['slug'])?>">
+									<?php echo esc_html($sub['name']);?>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					<?php endif; ?>
 				<?php 
 				endforeach; 
 			endif;
