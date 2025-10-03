898f57babb37ab2a082b1e43c01d0bfde396091d
nitu45641 <markutos987@gmail.com>
2025-10-03 20:43:41 +0600

    fix

--- templates/woo-filter/template-1/right-side/product-template.php
+++ templates/woo-filter/template-1/right-side/product-template.php
@@ -9,21 +9,21 @@ if (!function_exists('render_grid_product')) {
 function render_grid_product($product, $hide_prod_add_cart, $hide_prod_title, $hide_prod_desc, $hide_prod_rating, $hide_prod_price) {
 	?>
 	<div class="product-style product-style-<?php echo esc_attr($product['template']); ?>">
-		<a href="<?php echo esc_url($product['post_permalink']); ?>" target="_blank">
-			<div class="product-thumbnail">
+		<div class="product-thumbnail">
+			<a href="<?php echo esc_url($product['post_permalink']); ?>" target="_blank">
 				<div class="vpcc-image">
 					<?php echo $product['post_image']; ?>
 					<?php if (!empty($product['on_sale'])): ?>
 						<div class="badge on-sale-badge-<?php echo esc_attr($product['template']); ?>"><?php echo esc_html($product['on_sale_text']); ?></div>
 					<?php endif; ?>
 				</div>
-				<?php if( $hide_prod_add_cart == 'yes' ): ?>
-				<div class="card-action-btn-container">
-					<?php echo $product['cart_btn']; ?>
-				</div>
-				<?php endif; ?>
+			</a>
+			<?php if( $hide_prod_add_cart == 'yes' ): ?>
+			<div class="card-action-btn-container">
+				<?php echo $product['cart_btn']; ?>
 			</div>
-		</a>
+			<?php endif; ?>
+		</div>
 		<div class="product-content">
 			<?php if( $hide_prod_title == 'yes' ): ?>
 			<div class="product-name">
@@ -36,11 +36,6 @@ function render_grid_product($product, $hide_prod_add_cart, $hide_prod_title, $h
 			<?php if( $hide_prod_price == 'yes' ): ?>
 			<div class="product-price">
 				<?php echo $product['post_price']; ?>
-			</div>
-			<?php endif; ?>
-			<?php if( $hide_prod_add_cart == 'yes' ): ?>
-			<div class="cart cart-<?php echo esc_attr($product['template']); ?>">
-				<?php echo $product['cart_btn']; ?>
 			</div>
 			<?php endif; ?>
 		</div>
