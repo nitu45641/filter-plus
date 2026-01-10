3aec0c1c447e2bf25cddceebeeb2f1c991486721
nitu208 <82501062+nitu45641@users.noreply.github.com>
2026-01-10 14:14:09 +0600

    build filter layout

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cfd7426b78d9e2b0e329');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '2377fcb44ad7bc877e7c');
